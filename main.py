95ec33576d26bc5ad68d79a5296f3554dbe8ea94
Vicky-Di <vicky.di.zhang@gmail.com>
2024-10-27 21:32:31 +0100

    version_1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text, func
+from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
@@ -85,15 +85,15 @@ class PlanPost(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
     author = relationship("User", back_populates="plan_posts")
-    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(250), nullable=False)
     date: Mapped[str] = mapped_column(String(250), nullable=False)
     body: Mapped[str] = mapped_column(Text, nullable=False)
     img_url: Mapped[str] = mapped_column(String(250), nullable=True)
     category: Mapped[str] = mapped_column(String(250), nullable=False)
-    sub_category: Mapped[str] = mapped_column(String(250), nullable=False)
     comments = relationship("Comment", back_populates="parent_plan_post")
-    statues: Mapped[str] = mapped_column(String(250), nullable=False)
-    monates: Mapped[str] = mapped_column(String(500), nullable=False)
+    status: Mapped[str] = mapped_column(String(250), nullable=False)
+    month: Mapped[str] = mapped_column(String(500), nullable=False)
+    year: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
 
@@ -119,9 +119,15 @@ def select_posts(posts, category, sub_category):
         for post in posts:
             if post.category == category and post.sub_category == sub_category:
                 filtered_posts.append(post)
-
     return filtered_posts
 
+def select_plans(plans, month):
+    filtered_plans = []
+    if plans != None:
+        for plan in plans:
+            if month in plan.month:
+                filtered_plans.append(plan)
+    return filtered_plans
 
 
 # Use Werkzeug to hash the user's password when creating a new user.
@@ -154,7 +160,7 @@ def register():
     return render_template("register.html",form=form, buttonform = LoginbuttonForm(), logged_in=current_user.is_authenticated)
 
 
-# TODO: Retrieve a user from the database based on their email. 
+
 @app.route('/login', methods=["GET", "POST"])
 def login():
     form = LoginForm()
@@ -190,7 +196,26 @@ def get_all_posts():
     summer = select_posts(posts=all_posts, category='sparkle', sub_category='summer')[::-1][:3]
     winter = select_posts(posts=all_posts, category='sparkle', sub_category='winter')[::-1][:3]
     autumn = select_posts(posts=all_posts, category='sparkle', sub_category='autumn')[::-1][:3]
-    return render_template("index.html", spring=spring,summer=summer,winter=winter,autumn=autumn, logged_in=current_user.is_authenticated)
+    crochet = select_posts(posts=all_posts, category='hobby', sub_category='crochet')[::-1][:3]
+    sewing = select_posts(posts=all_posts, category='hobby', sub_category='sewing')[::-1][:3]
+    sashiko = select_posts(posts=all_posts, category='hobby', sub_category='sashiko')[::-1][:3]
+    books = select_posts(posts=all_posts, category='self-explore', sub_category='books')[::-1][:3]
+    courses = select_posts(posts=all_posts, category='self-explore', sub_category='courses')[::-1][:3]
+    professional = select_posts(posts=all_posts, category='self-explore', sub_category='professional')[::-1][:3]
+    all_plans = db.session.execute(db.select(PlanPost)).scalars().all()
+    january = select_plans(plans=all_plans, month='january')[::-1][:3]
+    february = select_plans(plans=all_plans, month='february')[::-1][:3]
+    march = select_plans(plans=all_plans, month='march')[::-1][:3]
+    april = select_plans(plans=all_plans, month='april')[::-1][:3]
+    may = select_plans(plans=all_plans, month='may')[::-1][:3]
+    june = select_plans(plans=all_plans, month='june')[::-1][:3]
+    july = select_plans(plans=all_plans, month='july')[::-1][:3]
+    august = select_plans(plans=all_plans, month='august')[::-1][:3]
+    september = select_plans(plans=all_plans, month='september')[::-1][:3]
+    october = select_plans(plans=all_plans, month='october')[::-1][:3]
+    november = select_plans(plans=all_plans, month='november')[::-1][:3]
+    december = select_plans(plans=all_plans, month='december')[::-1][:3]
+    return render_template("index.html", spring=spring,summer=summer,winter=winter,autumn=autumn,crochet=crochet,sewing=sewing,sashiko=sashiko, books=books, courses=courses,professional=professional, january = january,february = february,march = march,april = april,may = may,june = june,july = july,august = august,september = september,october = october,november = november,december = december,logged_in=current_user.is_authenticated)
 
 
 @app.route('/posts_list/<category>/<sub_category>/<logo>', methods=["GET", "POST"])
@@ -200,23 +225,44 @@ def get_select_posts(category, sub_category, logo):
     return render_template("post-list.html", all_posts= posts, logo=logo, category=category.capitalize(), sub_category=sub_category.capitalize(), logged_in=current_user.is_authenticated)
 
 
+@app.route('/plans_list/<month>/<logo>', methods=["GET", "POST"])
+def get_select_plans(month, logo):
+    all_plans = db.session.execute(db.select(PlanPost)).scalars().all()
+    plans = select_plans(plans=all_plans, month=month)
+    return render_template("plan-list.html", all_plans=plans,logo=logo, month=month.capitalize(), logged_in=current_user.is_authenticated)
+
+
 @app.route('/year_posts/<category>/<year>', methods=["GET", "POST"])
 def get_year_posts(category, year):
     condition = '%'+ year
     all_posts = db.session.execute(db.select(BlogPost).where(BlogPost.date.like(condition))).scalars().all()
-    print(all_posts)
     spring = select_posts(posts=all_posts, category='sparkle', sub_category='spring')[::-1]
-    print(spring)
     summer = select_posts(posts=all_posts, category='sparkle', sub_category='summer')[::-1]
     winter = select_posts(posts=all_posts, category='sparkle', sub_category='winter')[::-1]
     autumn = select_posts(posts=all_posts, category='sparkle', sub_category='autumn')[::-1]
     crochet = select_posts(posts=all_posts, category='hobby', sub_category='crochet')[::-1]
     sewing = select_posts(posts=all_posts, category='hobby', sub_category='sewing')[::-1]
     sashiko = select_posts(posts=all_posts, category='hobby', sub_category='sashiko')[::-1]
-    return render_template("year-posts.html", spring=spring,summer=summer,winter=winter,autumn=autumn,crochet=crochet,sewing=sewing,sashiko=sashiko, category=category.capitalize(), year=year,logged_in=current_user.is_authenticated)
+    books = select_posts(posts=all_posts, category='self-explore', sub_category='books')[::-1]
+    courses = select_posts(posts=all_posts, category='self-explore', sub_category='courses')[::-1]
+    professional = select_posts(posts=all_posts, category='self-explore', sub_category='professional')[::-1]
+    all_plans = db.session.execute(db.select(PlanPost)).scalars().all()
+    january = select_plans(plans=all_plans, month='january')[::-1]
+    february = select_plans(plans=all_plans, month='february')[::-1]
+    march = select_plans(plans=all_plans, month='march')[::-1]
+    april = select_plans(plans=all_plans, month='april')[::-1]
+    may = select_plans(plans=all_plans, month='may')[::-1]
+    june = select_plans(plans=all_plans, month='june')[::-1]
+    july = select_plans(plans=all_plans, month='july')[::-1]
+    august = select_plans(plans=all_plans, month='august')[::-1]
+    september = select_plans(plans=all_plans, month='september')[::-1]
+    october = select_plans(plans=all_plans, month='october')[::-1]
+    november = select_plans(plans=all_plans, month='november')[::-1]
+    december = select_plans(plans=all_plans, month='december')[::-1]
+    return render_template("year-posts.html", spring=spring,summer=summer,winter=winter,autumn=autumn,crochet=crochet,sewing=sewing,sashiko=sashiko, books=books, courses=courses,professional=professional, january = january,february = february,march = march,april = april,may = may,june = june,july = july,august = august,september = september,october = october,november = november,december = december,category=category.capitalize(), year=year,logged_in=current_user.is_authenticated)
 
 
-# TODO: Allow logged-in users to comment on posts
+
 @app.route("/post/<int:post_id>", methods=["GET", "POST"])
 def show_post(post_id):
     comment_form = CommentForm()
@@ -237,7 +283,26 @@ def show_post(post_id):
     return render_template("post.html", post=requested_post, logged_in=current_user.is_authenticated, form = comment_form)
 
 
-# TODO: Use a decorator so only an admin user can create a new post
+@app.route("/plan/<int:plan_id>", methods=["GET", "POST"])
+def show_plan(plan_id):
+    comment_form = CommentForm()
+    requested_plan = db.get_or_404(PlanPost, plan_id)
+    if comment_form.validate_on_submit():
+        if not current_user.is_authenticated:
+            flash("You need to login or register to comment.")
+            return redirect(url_for("login"))
+
+        new_comment = Comment(
+            text=comment_form.comment_text.data,
+            comment_author=current_user,
+            parent_plan=requested_plan,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_comment)
+        db.session.commit()
+    return render_template("plan.html", plan=requested_plan, logged_in=current_user.is_authenticated, form = comment_form)
+
+
 @app.route("/new-post/", methods=["GET", "POST"])
 @user_id_1_only
 def add_new_post():
@@ -257,7 +322,7 @@ def add_new_post():
         filename = secure_filename(file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
-        img_url = url_for('static', filename=f'uploads/{filename}')
+        img_url = f'uploads/{filename}'
 
         new_post = BlogPost(
             title=form.title.data,
@@ -265,8 +330,8 @@ def add_new_post():
             img_url=img_url,
             author_id=current_user.id,
             date=date.today().strftime("%B %d, %Y"),
-            main_option=form.main_option.data,
-            sub_option=form.sub_option.data
+            category=form.main_option.data,
+            sub_category=form.sub_option.data
         )
         db.session.add(new_post)
         db.session.commit()
@@ -279,29 +344,33 @@ def add_new_post():
 @user_id_1_only
 def add_new_plan():
     form = CreatePlanForm()
+
     if form.validate_on_submit():
         file = form.img.data
         filename = secure_filename(file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
-        img_url = url_for('static', filename=f'uploads/{filename}')
+        img_url = f'uploads/{filename}'
+        month = ','.join(form.month.data)
 
-        new_post = BlogPost(
+        new_plan = PlanPost(
             title=form.title.data,
             body=form.body.data,
             img_url=img_url,
             author_id=current_user.id,
             date=date.today().strftime("%B %d, %Y"),
-            main_option=form.main_option.data,
-            sub_option=form.sub_option.data
+            category=form.main_option.data,
+            status=form.status.data,
+            month = month,
+            year= form.year.data
         )
-        db.session.add(new_post)
+        db.session.add(new_plan)
         db.session.commit()
         return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form, logged_in=current_user.is_authenticated)
+    return render_template("make-plan.html", form=form, logged_in=current_user.is_authenticated)
 
 
-# TODO: Use a decorator so only an admin user can edit a post
+
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 @user_id_1_only
 def edit_post(post_id):
@@ -317,6 +386,7 @@ def edit_post(post_id):
         post.body = edit_form.body.data
         post.category = edit_form.main_option.data
         post.sub_category = edit_form.sub_option.data
+        post.img_url=edit_form.img_url.data
 
         if edit_form.img.data:
             file = edit_form.img.data
@@ -329,6 +399,73 @@ def edit_post(post_id):
         return redirect(url_for("show_post", post_id=post.id))
     return render_template("make-post.html", form=edit_form, is_edit=True, logged_in=current_user.is_authenticated)
 
+
+@app.route("/edit-plan/<int:plan_id>", methods=["GET", "POST"])
+@user_id_1_only
+def edit_plan(plan_id):
+    plan = db.get_or_404(PlanPost, plan_id)
+    edit_form = CreatePlanForm(
+        title=plan.title,
+        body=plan.body,
+        year=plan.year,
+        month=plan.month,
+        status=plan.status
+    )
+    
+    if edit_form.validate_on_submit():
+        plan.title = edit_form.title.data
+        plan.body = edit_form.body.data
+        plan.category = edit_form.main_option.data
+
+        if edit_form.img.data:
+            file = edit_form.img.data
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            plan.img_url = url_for('static', filename=f'uploads/{filename}')
+
+        db.session.commit()
+        return redirect(url_for("show_plan", plan_id=plan.id))
+    return render_template("make-plan.html", form=edit_form, is_edit=True, logged_in=current_user.is_authenticated)
+
+
+@app.route("/plan-to-post/<int:plan_id>", methods=["GET", "POST"])
+@user_id_1_only
+def plan_to_post(plan_id):
+    plan = db.get_or_404(PlanPost, plan_id)
+    form = CreatePostForm(
+        title=plan.title,
+        body=plan.body,
+        img_url=plan.img_url,
+        
+    )
+    if form.main_option.data == 'sparkle':
+        form.sub_option.choices = [('spring', 'Spring'), ('summer', 'Summer'), ('autumn', 'Autumn'),('winter', 'Winter')]
+    elif form.main_option.data == 'hobby':
+        form.sub_option.choices = [('crochet', 'Crochet'), ('sewing', 'Sewing'),('sashiko','Sashiko')]
+    elif form.main_option.data == 'self-explore':
+        form.sub_option.choices = [('books', 'Books'), ('courses', 'Courses'),('professional','Professional related')]
+
+    if form.validate_on_submit():
+        file = form.img.data
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        img_url = f'uploads/{filename}'
+
+        new_post = BlogPost(
+            title=form.title.data,
+            body=form.body.data,
+            img_url=img_url,
+            author_id=current_user.id,
+            date=date.today().strftime("%B %d, %Y"),
+            category=form.main_option.data,
+            sub_category=form.sub_option.data
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form, logged_in=current_user.is_authenticated)
 
 # TODO: Use a decorator so only an admin user can delete a post
 @app.route("/delete/<int:post_id>")
